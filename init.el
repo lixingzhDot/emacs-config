764f1f5b7979295a150f7fafc4ebfdcf69b026f6
Lixing Zhang <lixzha@microsoft.com>
2022-02-13 19:48:10 +0800

    commit from mac

--- init.el
+++ init.el
@@ -21,8 +21,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-	(zygospore projectile auto-org-md elfeed-org which-key use-package try org-bullets counsel auto-complete ace-window))))
+   '(zygospore projectile auto-org-md elfeed-org which-key use-package try org-bullets counsel auto-complete ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
