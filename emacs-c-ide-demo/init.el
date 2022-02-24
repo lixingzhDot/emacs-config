04587ccc2f77085e6456e7ca72b8de903f13b1be
Lixing Zhang <lixzha@microsoft.com>
2022-02-24 16:56:12 +0800

    remove unodtree

--- emacs-c-ide-demo/init.el
+++ emacs-c-ide-demo/init.el
@@ -38,7 +38,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
+    (zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
